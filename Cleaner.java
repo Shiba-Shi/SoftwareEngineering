f2cbbdddbd74b3dc437a1a10cdff017801c9a43c
Shiba-Shi <120398106+Shiba-Shi@users.noreply.github.com>
2022-12-29 22:32:39 +0800

    t17 t19
    update the link between t17 and t19、display of t19

---
+++ Cleaner.java
@@ -0,0 +1,68 @@
+package test;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
+import javax.swing.JTextArea;
+
+public class Cleaner extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Cleaner frame = new Cleaner();
+					if(CleanTable.needCleanedTable())
+						frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	
+	public void setLable(String tableNum, JTextArea textArea) {
+		textArea.append(tableNum+"\n");
+	}
+	
+	public Cleaner() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 436, 263);
+		contentPane.add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		JScrollPane jScrollPane = new JScrollPane(textArea);
+		panel.add(jScrollPane, BorderLayout.CENTER);
+		
+		for(String tableNum : CleanTable.newTable.needCleanedTableNum) {
+			setLable(tableNum, textArea);
+		}
+	}
+}
